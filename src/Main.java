da36bbd734b8d65a1bcb672e96558127511d82b9
Aleksei Sidorin <sidorinaleksei@gmail.com>
2021-02-17 20:00:58 +0300

    bonus calculator

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        int balance = 100;
+        int refill = 1200;
+        int bonus;
+
+
+        if (refill > 1000) {
+
+            bonus = refill / 100;
+
+        } else {
+
+            bonus = 0;
+        }
+
+        int sum = balance + refill + bonus;
+
+        System.out.println(sum);
+
+    }
+
+
+}
+
+
